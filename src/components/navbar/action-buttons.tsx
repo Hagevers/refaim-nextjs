bff52dba49215116a22e3c072a5e5c1b7333a5da
Amit Hadad <amoihadad1@gmail.com>
2024-02-19 13:43:37 +0200

    fixed mobile navigation, with donation button and logout/login

--- src/components/navbar/action-buttons.tsx
+++ src/components/navbar/action-buttons.tsx
@@ -11,14 +11,20 @@ import {
 import { AlignJustify } from "lucide-react";
 import Link from "next/link";
 import { Button } from "../ui/button";
-import { SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const ActionButtons = () => {
   return (
     <div>
       <div className="md:hidden">
         <Sheet>
-          <SheetTrigger>
+          <SheetTrigger className="text-white">
             <AlignJustify />
           </SheetTrigger>
           <SheetContent>
@@ -26,11 +32,23 @@ const ActionButtons = () => {
               <SheetDescription>
                 <div className="flex flex-col space-y-4 items-start w-full text-lg text-black mt-10 rtl:items-end">
                   <Link href="/">דף הבית</Link>
-                  <Link href="/donate">לתרומה</Link>
-                  <Link href="/signup">להצטרפות לעמותה</Link>
-                  <Link href="/login">להתחברות</Link>
-                  <Link href="/about">אודות</Link>
-                  <Link href="/contact">צור קשר</Link>
+                  <Link href="#contact">צור קשר</Link>
+                  <SignedIn>
+                    <Link href="/profile">לאיזור האישי</Link>
+                    <SignOutButton>להתנתק</SignOutButton>
+                  </SignedIn>
+                  <SignedOut>
+                    <SignInButton>
+                      <Link href="/signup">להצטרפות לעמותה</Link>
+                    </SignInButton>
+                  </SignedOut>
+                  <Link href="/donate">
+                    <div className="flex gap-4 justify-center pt-10">
+                      <Button className="bg-[#45684C] text-white px-10 py-2 rounded-md text-lg font-bold">
+                        לתרומה
+                      </Button>
+                    </div>
+                  </Link>
                 </div>
               </SheetDescription>
             </SheetHeader>
